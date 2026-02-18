c5372fa98f164e2ac577e8360800afd43de93f54
Andreas Høj <andrewhoj@gmail.com>
2026-02-18 11:36:14 +0100

    feat: fixed response format for refresh/access tokens

--- services/user/internal/dto/user.go
+++ services/user/internal/dto/user.go
@@ -6,8 +6,8 @@ type LoginRequestDTO struct {
 }
 
 type LoginResponseDTO struct {
-	AccessToken  string `json:"access_token,omitempty"`
-	RefreshToken string `json:"refresh_token,omitempty"`
+	AccessToken  string `json:"accessToken,omitempty"`
+	RefreshToken string `json:"refreshToken,omitempty"`
 }
 
 type RegisterUserDTO struct {
